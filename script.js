55ed4e6bf4e9f0aa937a978671e620dc65d29308
nyapblesso-a11y <nyapblesso@gmail.com>
2025-10-01 12:55:12 +0100

    updated UI and js for my first weather  app

--- script.js
+++ script.js
@@ -11,31 +11,28 @@ async function checkWeather (city) {
     document.querySelector('.error').style.display = 'block'
     document.querySelector('.weather').style.display = 'none'
   } else {
-    let data = await response.json()
+    const data = await response.json()
 
     document.querySelector('.city').innerHTML = data.name
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
     document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
 
-   if (data.weather[0].main === 'Clouds') {
-     weatherIcon.src ='./weather-images/clouds.png'
-   } else if (data.weather[0].main === 'Clear') {
-     weatherIcon.src = './weather-images/clear.png' 
-   } else if (data.weather[0].main === 'Rain') {
-     weatherIcon.src = './weather-images/rain.png' 
-   } else if (data.weather[0].main === 'Drizzle') {
-     weatherIcon.src = './weather-images/drizzle.png' 
-   } else if (data.weather[0].main === 'Mist') {
-     weatherIcon.src = './weather-images/mist.png' 
+    if (data.weather[0].main === 'Clouds') {
+      weatherIcon.src = './weather-images/clouds.png'
+    } else if (data.weather[0].main === 'Clear') {
+      weatherIcon.src = './weather-images/clear.png' 
+    } else if (data.weather[0].main === 'Rain') {
+      weatherIcon.src = './weather-images/rain.png' 
+    } else if (data.weather[0].main === 'Drizzle') {
+      weatherIcon.src = './weather-images/drizzle.png' 
+    }  else if (data.weather[0].main === 'Mist') {
+      weatherIcon.src = './weather-images/mist.png' 
+    }
+    document.querySelector('.weather').style.display = 'block'
+    document.querySelector('.error').style.display = 'none'
    }
-   document.querySelector('.weather').style.display = 'block'
-   document.querySelector('.error').style.display = 'none'
-  }
 }
-
-
 searchBnt.addEventListener('click', () => {
   checkWeather(searchBox.value)
 })
-  
