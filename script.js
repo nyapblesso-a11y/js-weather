72b096803176cfe1b7fb4118237d95b377cfbb06
nyapblesso-a11y <nyapblesso@gmail.com>
2025-10-07 12:09:46 +0100

    final commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function checkWeather (city) {
     document.querySelector('.weather').style.display = 'none'
   } else {
     const data = await response.json()
-    
+
     document.querySelector('.city').innerHTML = data.name
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
